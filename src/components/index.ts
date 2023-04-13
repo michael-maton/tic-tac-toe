08fceeb4af19c1474df6c966433381d2bcdbb873
michael-maton <michael@nonfungible.com>
2023-04-13 04:14:45 -0500

    AppBootstrap component created, App index logic moved there

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,2 @@
 export { default as Text } from './text/test';
+export { default as AppBootstrap } from './app-bootstrap/app-bootstrap';
--- src/index.tsx
+++ src/index.tsx
@@ -1,49 +1,9 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
-import * as SplashScreen from 'expo-splash-screen';
-import * as Font from 'expo-font';
-import { Text } from '@components';
+import React, { ReactElement } from 'react';
+import { Text, AppBootstrap } from '@components';
 
-// Keep the splash screen visible while we fetch resources
-SplashScreen.preventAutoHideAsync();
-
-export default function App() {
-  const [appIsReady, setAppIsReady] = useState(false);
-
-  useEffect(() => {
-    async function prepare() {
-      try {
-        // Pre-load fonts, make any API calls you need to do here
-        await Font.loadAsync({ Inter_400Regular, Inter_700Bold });
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        // Tell the application to render
-        setAppIsReady(true);
-      }
-    }
-
-    prepare();
-  }, []);
-
-  const onLayoutRootView = useCallback(async () => {
-    if (appIsReady) {
-      // This tells the splash screen to hide immediately! If we call this after
-      // `setAppIsReady`, then we may see a blank screen while the app is
-      // loading its initial state and rendering its first pixels. So instead,
-      // we hide the splash screen once we know the root view has already
-      // performed layout.
-      await SplashScreen.hideAsync();
-    }
-  }, [appIsReady]);
-
-  if (!appIsReady) {
-    return null;
-  }
-
+export default function App(): ReactElement {
   return (
-    <View style={styles.container} onLayout={onLayoutRootView}>
+    <AppBootstrap>
       <Text
         onPress={() => {
           alert('hello');
@@ -52,15 +12,6 @@ export default function App() {
       >
         Hello World!
       </Text>
-    </View>
+    </AppBootstrap>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});

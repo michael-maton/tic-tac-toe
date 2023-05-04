4baa737d5369167713a09bdd9a7d13d93a65a28f
michael-maton <michael@nonfungible.com>
2023-05-04 03:48:49 -0500

    createMultiplayerGame added to lambda function

--- amplify/backend/function/startGame/src/index.js
+++ amplify/backend/function/startGame/src/index.js
@@ -11,6 +11,7 @@ Amplify Params - DO NOT EDIT */
  */
 const appsync = require('aws-appsync');
 const gql = require('graphql-tag');
+// const { Expo } = require('expo-server-sdk');
 require('cross-fetch/polyfill');
 
 const credentials = process.env.AWS_EXECUTION_ENV.endsWith('mock')
@@ -38,7 +39,8 @@ exports.handler = async event => {
   const challenger = event.identity.username;
   const invitee = event.arguments.invitee;
 
-  const query = gql`
+  // 1. Make sure challenger and invitee exist
+  const userQuery = gql`
     query getUser($username: String!) {
       getUser(username: $username) {
         id
@@ -47,14 +49,14 @@ exports.handler = async event => {
   `;
 
   const challengerResponse = await graphqlClient.query({
-    query,
+    query: userQuery,
     variables: {
       username: challenger
     }
   });
 
   const inviteeResponse = await graphqlClient.query({
-    query,
+    query: userQuery,
     variables: {
       username: invitee
     }
@@ -70,11 +72,68 @@ exports.handler = async event => {
     throw new Error('You cannot invite yourself.');
   }
 
+  // 2. Creating a new Game
+  const gameMutation = gql`
+    mutation createGame(
+      $status: GameStatus!
+      $users: [String!]!
+      $owner: String!
+      $turn: String!
+      $state: [GameSymbol]!
+    ) {
+      createGame(input: { status: $status, users: $users, owner: $owner, turn: $turn, state: $state }) {
+        id
+        status
+        state
+        turn
+        winner
+      }
+    }
+  `;
+
+  const gameResponse = await graphqlClient.mutate({
+    mutation: gameMutation,
+    variables: {
+      status: 'REQUESTED',
+      users: [challenger, invitee],
+      owner: challenger,
+      turn: Math.random() < 0.5 ? challenger : invitee,
+      state: [null, null, null, null, null, null, null, null, null]
+    }
+  });
+
+  //   // 3. Linking the Game with the Users (by creating a MultiplayerGame model)
+  const multiplayerGameMutation = gql`
+    mutation createMultiplayerGame($gameID: ID!, $userUsername: String!, $users: [String!]!) {
+      createMultiplayerGame(input: { gameID: $gameID, userUsername: $userUsername, users: $users }) {
+        id
+      }
+    }
+  `;
+
+  const challengerMultiplayerGameResponse = await graphqlClient.mutate({
+    mutation: multiplayerGameMutation,
+    variables: {
+      gameID: gameResponse.data.createGame.id,
+      userUsername: challenger,
+      users: [challenger, invitee]
+    }
+  });
+
+  const inviteeMultiplayerGameResponse = await graphqlClient.mutate({
+    mutation: multiplayerGameMutation,
+    variables: {
+      gameID: gameResponse.data.createGame.id,
+      userUsername: invitee,
+      users: [challenger, invitee]
+    }
+  });
+
   return {
-    id: '12323',
-    status: 'REQUESTED',
-    turn: 'test',
-    state: ['x', 'o', null],
-    winner: 'test'
+    id: gameResponse.data.createGame.id,
+    status: gameResponse.data.createGame.status,
+    state: gameResponse.data.createGame.state,
+    turn: gameResponse.data.createGame.turn,
+    winner: gameResponse.data.createGame.winner
   };
 };

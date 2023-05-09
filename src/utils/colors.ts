5e6f3ca53090a09ef467cc37adba07c2ba7bf3ea
michael-maton <michael@nonfungible.com>
2023-05-09 15:04:36 -0500

    Colors, @api util

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -5,7 +5,10 @@ const colors = {
   lightBlueOrig: '#687a90',
   lightBlue: '#788A9E',
   darkBlue: '#112841',
-  gold: '#fce38a'
+  gold: '#fce38a',
+  win: '#86ff7d',
+  loss: '#ff937d',
+  draw: '#82cfff'
 };
 
 export default colors;
